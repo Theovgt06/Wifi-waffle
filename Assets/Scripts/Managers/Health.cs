0d1cb1a49108150a695d5ebfba8b14ede3daf0f0
Theovgt06 <theofromental@gmail.com>
2025-04-03 11:54:25 +0200

    Ammo
    Ammo system (à update avec les prochains push)

--- Assets/Scripts/Managers/Health.cs
+++ Assets/Scripts/Managers/Health.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    [SerializeField] private const int MAX_HEALTH = 7;
+    [SerializeField] private int maxHealth = 7;
     [SerializeField] private int currentHealth;
     public void ChangeHealth(int amount)
     {
         currentHealth += amount;
-        if (currentHealth > MAX_HEALTH)
+        if (currentHealth > maxHealth)
         {
-            currentHealth = MAX_HEALTH;
+            currentHealth = maxHealth;
         }
         else if (currentHealth <= 0)
         {
