36b9dc47fd34119a031fe82efa0fac16bb592f80
Theovgt06 <theofromental@gmail.com>
2025-04-03 12:01:39 +0200

    Update PlateformBehavior.cs

--- Assets/Scripts/Plateforming/PlateformBehavior.cs
+++ Assets/Scripts/Plateforming/PlateformBehavior.cs
@@ -3,16 +3,22 @@ using UnityEngine;
 public class PlateformBehavior : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    enum BehaviorBiome {Easy, Normal, Hard}
+    enum BehaviorBiome { Easy, Normal, Hard }
+
+    private PlatformEffector2D platformEffector;
 
     void Start()
     {
-        
+        platformEffector = GetComponent<PlatformEffector2D>();
+        if (platformEffector != null)
+        {
+            platformEffector.useOneWay = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
