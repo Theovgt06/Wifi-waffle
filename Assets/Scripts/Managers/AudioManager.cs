389ed3ca55bc226d065e522a1f1ff7ed56d1ee00
Theovgt06 <theofromental@gmail.com>
2025-04-14 16:04:06 +0200

    AudioManager + MainMenu music

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("---------------Audio Sources---------------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SfxSource;
+
+    [Header("---------------Audio Clip---------------")]
+    public AudioClip backgroundmusic;
+    public AudioClip backgroundnoise;
+    public AudioClip death;
+    public AudioClip walk;
+    public AudioClip jump;
+    public AudioClip damageTaken;
+    public AudioClip landing;
+    public AudioClip shooting;
+    public AudioClip screenWarp;
+    public AudioClip itemCollected;
+    public AudioClip buttons;
+    public AudioClip enemyDamaged;
+    public AudioClip spotted;
+
+    private void Start()
+    {
+        PlayMusic();
+    }
+
+    public void PlayMusic()
+    {
+        if (musicSource != null && backgroundmusic != null)
+        {
+            Debug.Log("PlayMusic() appelé");
+            musicSource.clip = backgroundmusic;
+         
+            musicSource.loop = true; // Set the music to loop
+            
+            musicSource.Play();
+        }
+        else
+        {
+            Debug.LogWarning("MusicSource ou Backgroundmusic n'est pas assigné !");
+        }
+    }
+
+    public void PlaySfx(AudioClip clip)
+    {
+       SfxSource.PlayOneShot(clip);
+    }
+}
